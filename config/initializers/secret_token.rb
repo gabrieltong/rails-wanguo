9983222fde20f2c6071c302a9b20db551c1ae0ef
gabrieltong <tongyangmath@gmail.com>
2013-09-04 17:17:02 +0800

    init coomit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsWanguo::Application.config.secret_token = 'c7de6a057aaffb257a8b8574a4b62935e8da0974208f1b5bfd39d1a37dcc65a644d6b6220cc8fb9261278b1caaa5a96af9de9d326af37be7366ebdfceb18119f'
