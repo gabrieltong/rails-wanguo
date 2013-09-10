b83d40318ffc7d9b7f345c05a5068d0890d88270
tongyang <tongyangmath@gmail.com>
2013-09-10 16:04:12 +0800

    导入知识点功能

--- app/models/question.rb
+++ app/models/question.rb
@@ -3,12 +3,12 @@ class Question < ActiveRecord::Base
   serialize :choices
 
   has_many :ep_questions
-  has_many :a_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'A'},:class_name => "Exampoint"
-  has_many :b_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'B'},:class_name => "Exampoint"
-  has_many :c_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'C'},:class_name => "Exampoint"
-  has_many :d_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'D'},:class_name => "Exampoint"
-  has_many :e_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'E'},:class_name => "Exampoint"
-  has_many :f_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'F'},:class_name => "Exampoint"
-  has_many :g_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'G'},:class_name => "Exampoint"
-  has_many :h_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'H'},:class_name => "Exampoint"
+  has_many :a_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'A'},:source => :exampoint
+  has_many :b_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'B'},:source => :exampoint
+  has_many :c_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'C'},:source => :exampoint
+  has_many :d_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'D'},:source => :exampoint
+  has_many :e_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'E'},:source => :exampoint
+  has_many :f_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'F'},:source => :exampoint
+  has_many :g_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'G'},:source => :exampoint
+  has_many :h_eps,:through=>:ep_questions,:conditions=>{"ep_questions.state"=>'H'},:source => :exampoint
 end
