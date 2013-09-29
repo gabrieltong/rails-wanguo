dfca3679d55d35f66baaa8c63fc0a01211206914
gabrieltong <tongyangmath@gmail.com>
2013-09-29 16:32:54 +0800

    i添加istudy模型

---
+++ app/models/istudy.rb
@@ -0,0 +1,22 @@
+# encoding: UTF-8
+class Istudy
+  Map = [
+    {:title=>'民法',:radio=>0.16,:law=>0.2,:law_cost=>96,:question_cost=>18},
+    {:title=>'刑法',:radio=>0.16,:law=>0.2,:law_cost=>96,:question_cost=>15},
+    {:title=>'行政法',:radio=>0.08,:law=>0.2,:law_cost=>48,:question_cost=>8},
+    {:title=>'法理学',:radio=>0.06,:law=>0.5,:law_cost=>10000,:question_cost=>9},
+    {:title=>'法制史',:radio=>0.02,:law=>0.5,:law_cost=>10000,:question_cost=>4},
+    {:title=>'宪法',:radio=>0.03,:law=>0.5,:law_cost=>18,:question_cost=>9},
+    {:title=>'司法制度',:radio=>0.02,:law=>0.5,:law_cost=>12,:question_cost=>5},
+    {:title=>'商法',:radio=>0.08,:law=>0.5,:law_cost=>48,:question_cost=>13},
+    {:title=>'经济法',:radio=>0.07,:law=>0.5,:law_cost=>42,:question_cost=>12},
+    {:title=>'刑诉',:radio=>0.12,:law=>0.8,:law_cost=>72,:question_cost=>40},
+    {:title=>'民诉',:radio=>0.12,:law=>0.8,:law_cost=>72,:question_cost=>45},
+    {:title=>'国际法',:radio=>0.02,:law=>0.8,:law_cost=>12,:question_cost=>10},
+    {:title=>'国际私法',:radio=>0.03,:law=>0.8,:law_cost=>18,:question_cost=>13},
+    {:title=>'国际经济法',:radio=>0.03,:law=>0.8,:law_cost=>18,:question_cost=>15}
+  ]
+  def self.summary(user)
+    
+  end
+end
