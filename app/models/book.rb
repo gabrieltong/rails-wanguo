e40d68e167363e4e4988752722c332330c874d7b
tongyang <tongyang@tongyangs-Mac-mini.local>
2013-12-07 11:10:38 +0800

    样式修改

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,11 +2,13 @@ class Book < ActiveRecord::Base
   attr_accessible :author, :details, :press, :price, :taobao, :title,:file,:published_at
   has_attached_file :file
 
+  validates :author, :details, :press, :price, :taobao, :title,:file,:presence=>true
+  validates_attachment :file,:presence=>true
   def as_json(options={})
     super({:methods=>[:file_url]}.merge options)
   end
 
-  def file_url
+	def file_url
     file.url
   end
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -2,6 +2,8 @@ class School < ActiveRecord::Base
   attr_accessible :details, :title, :url,:file,:city,:price,:started_at
   has_attached_file :file
 
+  validates :details, :title, :url,:file,:city,:price,:started_at,:presence=>true
+  validates_attachment :file,:presence=>true
   def as_json(options={})
     super({:methods=>[:file_url]}.merge options)
   end
