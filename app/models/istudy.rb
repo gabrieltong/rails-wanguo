46d99d18bde4c06358c63a32e4b928342ca9d37f
tongyang <tongyang@tongyangs-Mac-mini.local>
2013-12-07 11:53:52 +0800

    修改Istudy配置

--- app/models/istudy.rb
+++ app/models/istudy.rb
@@ -1,20 +1,22 @@
 # encoding: UTF-8
 class Istudy
-  Map = [
-    {:title=>'民法',:ratio=>0.16,:law_ratio=>0.2,:law_cost=>96,:question_cost=>18},
-    {:title=>'刑法',:ratio=>0.16,:law_ratio=>0.2,:law_cost=>96,:question_cost=>15},
-    {:title=>'行政法',:ratio=>0.08,:law_ratio=>0.2,:law_cost=>48,:question_cost=>8},
-    {:title=>'法理学',:ratio=>0.06,:law_ratio=>0.5,:law_cost=>10000,:question_cost=>9},
-    {:title=>'法制史',:ratio=>0.02,:law_ratio=>0.5,:law_cost=>10000,:question_cost=>4},
-    {:title=>'宪法',:ratio=>0.03,:law_ratio=>0.5,:law_cost=>18,:question_cost=>9},
-    {:title=>'司法制度',:ratio=>0.02,:law_ratio=>0.5,:law_cost=>12,:question_cost=>5},
-    {:title=>'商法',:ratio=>0.08,:law_ratio=>0.5,:law_cost=>48,:question_cost=>13},
-    {:title=>'经济法',:ratio=>0.07,:law_ratio=>0.5,:law_cost=>42,:question_cost=>12},
-    {:title=>'刑诉',:ratio=>0.12,:law_ratio=>0.8,:law_cost=>72,:question_cost=>40},
-    {:title=>'民诉',:ratio=>0.12,:law_ratio=>0.8,:law_cost=>72,:question_cost=>45},
-    {:title=>'国际法',:ratio=>0.02,:law_ratio=>0.8,:law_cost=>12,:question_cost=>10},
-    {:title=>'国际私法',:ratio=>0.03,:law_ratio=>0.8,:law_cost=>18,:question_cost=>13},
-    {:title=>'国际经济法',:ratio=>0.03,:law_ratio=>0.8,:law_cost=>18,:question_cost=>15}
+  Map = [ 
+    {:title=>'社会主义法治理念',:radio=> 1,:law_ratio=>20,:law_cost=>10000 ,:question_cost=>5 },
+    {:title=>'法理学',:radio=> 6,:law_ratio=>50,:law_cost=>10000 ,:question_cost=>9 },
+    {:title=>'法制史',:radio=> 2,:law_ratio=>50,:law_cost=>10000 ,:question_cost=>4 },
+    {:title=>'宪法',:radio=> 3,:law_ratio=>50,:law_cost=>18,:question_cost=>9 },
+    {:title=>'经济法',:radio=> 7,:law_ratio=>50,:law_cost=>42,:question_cost=>12},
+    {:title=>'国际法',:radio=> 2,:law_ratio=>80,:law_cost=>12,:question_cost=>10},
+    {:title=>'国际私法',:radio=> 3,:law_ratio=>80,:law_cost=>18,:question_cost=>13},
+    {:title=>'国际经济法',:radio=> 3,:law_ratio=>80,:law_cost=>18,:question_cost=>15},
+    {:title=>'司法制度和法律职业道德',:radio=> 1,:law_ratio=>50,:law_cost=>12,:question_cost=>5 },
+    {:title=>'刑法',:radio=>16,:law_ratio=>20,:law_cost=>96,:question_cost=>15},
+    {:title=>'刑事诉讼法',:radio=>12,:law_ratio=>80,:law_cost=>72,:question_cost=>40},
+    {:title=>'行政法与行政诉讼法',:radio=> 8,:law_ratio=>20,:law_cost=>48,:question_cost=>8 },
+    {:title=>'民法',:radio=>16,:law_ratio=>20,:law_cost=>96,:question_cost=>18},
+    {:title=>'商法',:radio=> 8,:law_ratio=>50,:law_cost=>48,:question_cost=>13},
+    {:title=>'民事诉讼法与仲裁制度',:radio=>12,:law_ratio=>80,:law_cost=>72,:question_cost=>45},
+    
   ]
 
   def self.cache_complex
