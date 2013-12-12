bab13c829e2c8c800f319feb969613c88370c10a
gabrieltong <tongyangmath@gmail.com>
2013-12-12 13:52:34 +0800

    update config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -36,13 +36,13 @@ RailsWanguo::Application.configure do
   config.assets.debug = true
 
   config.action_mailer.delivery_method = :smtp
-  
+
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
     domain:               'example.com',
     user_name:            'tongyangmath',
-    password:             'dlutlms',
+    password:             '',
     authentication:       'plain',
     enable_starttls_auto: true  
   }
