50a3aa417cfefd3dbcbd9e55d106f0f4ced9e8a9
gabrieltong <tongyangmath@gmail.com>
2013-12-12 13:50:23 +0800

    update config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,16 @@ RailsWanguo::Application.configure do
 
   # Expands the lines which load the assets
   config.assets.debug = true
+
+  config.action_mailer.delivery_method = :smtp
+  
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'tongyangmath',
+    password:             'dlutlms',
+    authentication:       'plain',
+    enable_starttls_auto: true  
+  }
 end
