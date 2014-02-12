5e51f1c4231326d50934b23e50c406f2b544ffb0
Gabriel Tong <tongyangmath@gmail.com>
2014-02-12 10:53:54 +0800

    添加500人僵尸账号

--- app/models/istudy.rb
+++ app/models/istudy.rb
@@ -127,7 +127,7 @@ class Istudy
 
   # 综合实力排名
   def self.complex_rank(user)
-    {:total=>[User.count(),500].max, :rank=>User.where("complex > ?",user.complex).count()+1,:value=>user.complex}
+    {:total=>User.count()+500, :rank=>User.where("complex > ?",user.complex).count()+1+500,:value=>user.complex}
   end
 
   # 综合能力数值
