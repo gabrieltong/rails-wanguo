2ac3aa565e6568524d0a9ceec9ef9421fb2ddfa7
Gabriel <tongyangmath@gmail.com>
2014-06-06 16:20:44 +0800

    修改验证

--- app/models/import.rb
+++ app/models/import.rb
@@ -24,11 +24,12 @@ class Import < ActiveRecord::Base
   #   Import.laws_zip.each {|i|i.import}
   # end
 
-  validate :title,:presence=>true,:if=>"state == 'laws' || state == 'freelaws' || question_kind == 'moni'"
+  
   validate :validate_laws,:if=>"state == 'laws'"
   validate :validate_freelaws,:if=>"state == 'freelaws'"
   validate :validate_questions,:if=>"state == 'questions'"
   validate :validate_eps,:if=>"state == 'eps'"
+  validates :title,:presence=>true,:if=>"state == 'laws' || state == 'freelaws' || question_kind == 'scope_moni'"
 
   validates_attachment :file, :content_type => {
           :content_type=>["application/vnd.ms-excel",   
