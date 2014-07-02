833892bafe2448c6319e654a2c9cd17229609a16
Gabriel Tong <tongyangmath@gmail.com>
2014-07-02 11:49:23 +0800

    更新apk地址

--- app/controllers/entrance_controller.rb
+++ app/controllers/entrance_controller.rb
@@ -13,9 +13,7 @@ class EntranceController < ApplicationController
   def download_link
 
 	user_agent = UserAgent.parse(request.user_agent)
-  ppp user_agent
-  ppp user_agent.platform
-	if user_agent.platform.downcase == 'ipad' || user_agent.platform.downcase == 'ipone'
+	if user_agent.platform.downcase == 'ipad' || user_agent.platform.downcase == 'iphone' || user_agent.platform.downcase == 'ipod touch' 
 		redirect_to 'http://mp.weixin.qq.com/mp/redirect?url=https://itunes.apple.com/cn/app/wan-guo-si-kao/id852759260?mt=8'
 	else
 		redirect_to 'http://m.app.so.com/detail/index?pname=sevencolors.android.wanguo&id=1579992'
